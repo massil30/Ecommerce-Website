413d3eb3934a0989ee8e749917b142624ad49dbe
massil30 <massilattaf04@gmail.com>
2025-11-03 01:11:05 +0100

    Fix responsivity using Clamp and Hide elements

--- Frontend/src/header.jsx
+++ Frontend/src/header.jsx
@@ -6,7 +6,7 @@ const Header = () => {
 
   return (
     <header className="sticky top-0 z-50 mt-0 p-0 bg-white shadow-sm w-full">
-      <div className="flex items-center justify-between w-full  md:px-12">
+      <div className="flex items-center justify-between w-full  ">
         {/* LEFT SECTION */}
         <div className="flex items-center gap-3">
           <img
@@ -15,7 +15,7 @@ const Header = () => {
             className="w-10 h-10 object-contain"
           />
           <h1
-            className="text-2xl font-bold"
+            className="text-2xl md,sm:text-xl font-bold"
             style={{ fontFamily: "Montserrat, sans-serif" }}
           >
             Furniro
@@ -23,12 +23,12 @@ const Header = () => {
         </div>
 
         {/* CENTER NAVIGATION */}
-        <nav className="hidden md:flex items-center gap-16">
+<nav className="items-center hidden md:flex gap-[clamp(1rem,4vw,3rem)]">
           {["Home", "Shop", "About", "Contact"].map((item) => (
             <a
               key={item}
               href={`#${item.toLowerCase()}`}
-              className="text-[1rem] font-medium text-gray-700 hover:text-blue-600 transition-colors"
+              className="text-[1rem] md:0.8rem font-medium text-gray-700 hover:text-blue-600 transition-colors"
               style={{ fontFamily: "Poppins, sans-serif" }}
             >
               {item}
@@ -37,7 +37,7 @@ const Header = () => {
         </nav>
 
         {/* RIGHT ICONS */}
-        <div className="hidden md:flex items-center gap-8">
+        <div className="hidden  sm:flex items-center md:gap-3 sm:gap-4 lg:gap-6">
           <User className="w-6 h-6 text-gray-700 cursor-pointer hover:text-blue-600" />
           <Search className="w-6 h-6 text-gray-700 cursor-pointer hover:text-blue-600" />
           <Heart className="w-6 h-6 text-gray-700 cursor-pointer hover:text-blue-600" />
@@ -46,7 +46,7 @@ const Header = () => {
 
         {/* MOBILE MENU BUTTON */}
         <button
-          className="md:hidden flex items-center"
+          className="sm:hidden flex items-center"
           onClick={() => setOpen(!open)}
         >
           {open ? <X size={28} /> : <Menu size={28} />}
@@ -55,7 +55,7 @@ const Header = () => {
 
       {/* MOBILE DRAWER MENU */}
       {open && (
-        <div className="md:hidden bg-white border-t border-gray-200 py-4 px-6 space-y-4">
+        <div className="sm:hidden bg-white border-t border-gray-200 py-4 px-6 space-y-4">
           {["Home", "Shop", "About", "Contact"].map((item) => (
             <a
               key={item}
